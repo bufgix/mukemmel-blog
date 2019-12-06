e7e0b0b72e204255dba9126b8528d20c8f2280b7
bufgix <ooruc471@yandex.com>
2019-12-07 00:53:28 +0300

    added express server and google auth

---
+++ server/index.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const next = require("next");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth2");
+const dotenv = require("dotenv").config();
+
+const dev = process.env.NODE_ENV !== "production";
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
+
+const PORT = process.env.PORT | 3000;
+
+nextApp.prepare().then(() => {
+  const app = express();
+
+  // Body parser
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+
+  app.get("/hey", (req, res) => {
+    res.status(200).json({ hello: "hello world" });
+  });
+
+  // Passport configure
+  app.use(passport.initialize());
+  app.use(passport.session());
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: process.env.GOOGLE_CALLBACK_URL
+      },
+      (accessToken, refreshToken, profile, done) => {
+        console.log(JSON.stringify(profile));
+        done(null, profile);
+      }
+    )
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user);
+  });
+
+  passport.deserializeUser((user, done) => {
+    done(null, user);
+  });
+
+  app.get(
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["profile"]
+    })
+  );
+
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google", { scope: ["profile"] }),
+    (req, res) => {
+      res.redirect("/");
+    }
+  );
+
+  app.get("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  app.listen(PORT, err => {
+    if (err) throw err;
+    console.log(`Express ready on > ${PORT}`);
+  });
+});
