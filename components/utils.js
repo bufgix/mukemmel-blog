40b03ea1171f163336ed60a317dcea90a0d2c2aa
bufgix <ooruc471@yandex.com>
2019-12-11 18:57:48 +0300

    fix date

--- components/utils.js
+++ components/utils.js
@@ -1,8 +1,8 @@
 export const parseDate = dateStr => {
-  const d = new Date(dateStr),
-    month = "" + (d.getMonth() + 1),
-    day = "" + d.getDate(),
-    year = d.getFullYear();
+  const d = new Date(dateStr);
+  let month = "" + (d.getMonth() + 1);
+  let day = "" + d.getDate();
+  const year = d.getFullYear();
 
   if (month.length < 2) month = "0" + month;
   if (day.length < 2) day = "0" + day;
